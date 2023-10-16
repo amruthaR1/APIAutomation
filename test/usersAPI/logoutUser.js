605f62652e399f2eeeae83a58aaaec0e27817f7f
amrutha <amruthar@testvagrant.com>
2023-10-16 12:45:14 +0530

    added testcase for logout

---
+++ test/usersAPI/logoutUser.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+const chai = require("chai");
+const expect = chai.expect;
+const baseURL = require("../../data/baseURL.json");
+const EndPoint = require("../../data/endPoints.json");
+const token = require("../../data/authorizationToken.json");
+
+
+describe('Logout User', () => {
+    it('Should logout the user', async()=>{
+        const authToken = token.CorrectToken;
+        const config = {
+          headers: { Authorization: `Bearer ${authToken}` },
+        };
+        const response = await axios.get(
+          baseURL.BaseURL + EndPoint.UserEndpoint.LogOut,
+          config
+        );
+        expect(response.status).to.be.equal(200);
+    })
+    
+});
